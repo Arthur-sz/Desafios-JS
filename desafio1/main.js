6df09b7ad1294c7bbf573a39dc2dbef5629127e0
ARTHUR SOUZA <2025101367@academicougb.com.br>
2026-05-21 11:19:50 -0300

    adiciona estrutura javascript

---
+++ desafio1/main.js
@@ -0,0 +1,13 @@
+let valor1 = document.getElementById("valor1")
+let valor2 = document.getElementById("valor2")
+
+let calcular = document.getElementById("calcular")
+
+let soma = document.getElementById("soma")
+let subtracao = document.getElementById("subtracao")
+let multiplicacao =  document.getElementById("multiplicacao")
+let divisao = document.getElementById("divisao")
+
+calcular.addEventListener("click", function(){
+
+})
