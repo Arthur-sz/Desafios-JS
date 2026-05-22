cfb431c6ba4137042b68ecf4d3310383791fb1af
Arthur <arthurvr723@gmail.com>
2026-05-21 21:12:13 -0300

    finaliza a estrutura lógica do java script

--- desafio1/main.js
+++ desafio1/main.js
@@ -10,4 +10,15 @@ let divisao = document.getElementById("divisao")
 
 calcular.addEventListener("click", function(){
 
+let numero1 = Number(valor1.value)
+let numero2 = Number(valor2.value)
+
+soma.innerHTML = "Resultado da soma: " + (numero1 + numero2)
+subtracao.innerHTML = "Resultado da subtração: " + (numero1 - numero2)
+multiplicacao.innerHTML = "Resultado da multipicação: " + (numero1 * numero2)
+if (numero2 === 0){
+    divisao.innerHTML = "Resultado da divisão: O valor não pode ser 0" 
+} else {
+    divisao.innerHTML = "Resultado da divisão: " + (numero1 / numero2)
+}
 })
